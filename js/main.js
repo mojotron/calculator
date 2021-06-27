ec1751308eda1a132d5dd1db7f33f154886e74e5
mojotron <igormalj@gmail.com>
2021-06-27 22:06:33 +0200

    Add clear and clearAll button logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 "use strict";
-//DOM Selectors
 const numbers = document.querySelectorAll(".btn-number");
 const operators = document.querySelectorAll(".btn-operator");
 const display = document.querySelector(".display");
@@ -7,160 +6,18 @@ const floatPoint = document.querySelector(".btn-dot");
 const openPara = document.querySelector(".btn-open-parentheses");
 const closePara = document.querySelector(".btn-close-parentheses");
 const equal = document.querySelector(".btn-equal");
-// INPUT LOGIC - goal of this code is to stop user for
-// inputting badly formatted math, by not allowing inputs
-//(Example cant start with close para or multiple dots in float number )
+const deleteChar = document.querySelector(".btn-del-one");
+const reset = document.querySelector(".btn-del-all");
 
-let numberFlag = false;
-let dotFlag = true;
-let negationFlag = false;
-let inputs = [];
-let paraCounter = 0;
-let currType = "";
-let currValue = "";
-
-const addInput = (value) => inputs.push(value);
-
-function resetNumberAndDotFlag() {
-  numberFlag = false;
-  dotFlag = true;
-}
-
-function updateNumber(e) {
-  currValue += e.target.textContent;
-  updateDisplay(e);
-}
-
-function updateDisplay(event) {
-  display.textContent += event.target.textContent;
-}
-function removeDot() {
-  //When user enters . but no numbers after it, remove it from display
-  if (display.textContent.slice(-1) === ".") {
-    display.textContent = display.textContent.slice(0, -1);
-  }
-}
-
-for (let num of numbers) {
-  //Number is possible to enter if it is first char,after operator or open parentheses
-  num.addEventListener("click", function (e) {
-    if (["", "operator", "openPara", "number"].includes(currType)) {
-      if (currType !== "number" && currValue !== "") addInput(currValue);
-      if (currType !== "number") currValue = "";
-      //For negative numbers
-      if (negationFlag) {
-        currValue = "-";
-        negationFlag = false;
-      }
-      numberFlag = true;
-      currType = "number";
-      updateNumber(e);
-    }
-  });
-}
-floatPoint.addEventListener("click", function (e) {
-  if (numberFlag && dotFlag) {
-    updateNumber(e);
-    dotFlag = false;
-  }
-});
-
-for (let ope of operators) {
-  // Operator is possible if after number or close parentheses
-  ope.addEventListener("click", function (e) {
-    if (currValue !== "" && currType !== "operator") addInput(currValue);
-    resetNumberAndDotFlag();
-    // Minus operator is used for indicating negative number or negating next parentheses
-    if (e.target.textContent === "-") {
-      if (currValue === "" || currType === "openPara") {
-        if (!negationFlag) updateDisplay(e);
-        negationFlag = true;
-        currValue = ""; //For bug with inputing double open para in -(-num) then currValue !== "" in num check
-        return;
-      }
-    }
-    if (currType === "number" || currType === "closePara") {
-      currType = "operator";
-      currValue = e.target.textContent;
-      removeDot();
-      updateDisplay(e);
-      return;
-    }
-  });
-}
-
-openPara.addEventListener("click", function (e) {
-  //Open parentheses only if first char on screen, if it is after operator
-  //or after another open parentheses
-  if (negationFlag) {
-    //two edge cases if minus is before or after
-    addInput("-");
-    negationFlag = false;
-  }
-  resetNumberAndDotFlag();
-  if (
-    ["", "operator", "openPara"].includes(currType) ||
-    display.textContent === "-"
-  ) {
-    if (currValue !== "") addInput(currValue);
-    paraCounter += 1;
-    currType = "openPara";
-    currValue = e.target.textContent;
-    updateDisplay(e);
-    return;
-  }
-});
-
-closePara.addEventListener("click", function (e) {
-  //Close parentheses is possible after the number or another close parentheses
-  if (paraCounter === 0) return;
-  if (currType === "number" || currType === "closePara") {
-    if (currValue !== "") addInput(currValue);
-    paraCounter -= 1;
-    currType = "closePara";
-    currValue = e.target.textContent;
-    updateDisplay(e);
-    return;
-  }
-});
-
-equal.addEventListener("click", function () {
-  if (currValue !== "") addInput(currValue);
-  const result = eval(display.textContent);
-  display.textContent = result;
-  currType = "number";
-  currValue = result;
-  inputs = [];
-});
-
-//Solve math with infix expression
-class Stack {
-  constructor() {
-    this.stack = [];
-  }
-  isEmpty() {
-    return this.stack.length === 0;
-  }
-  push(value) {
-    this.stack.push(value);
-    return this.stack.length;
-  }
-  pop() {
-    if (this.isEmpty()) return null;
-    return this.stack.pop();
-  }
-  top() {
-    if (this.isEmpty()) return null;
-    return this.stack[this.stack.length - 1];
-  }
-}
-
-/////const
-expression = {
+const expression = {
   elements: [],
   countParas: 0,
   insert: function (value, type) {
     this.elements.push({ value, type });
+  },
+  removeLast: function () {
+    if (this.isEmpty()) return null;
+    return this.elements.pop();
   },
   isEmpty: function () {
     return this.elements.length === 0;
@@ -175,6 +32,13 @@ expression = {
   lastType: function () {
     return this.lastElement().type;
   },
+  printExpression: function () {
+    return this.elements.map((ele) => ele.value).join(" ");
+  },
+  resetExpression: function () {
+    this.elements = [];
+    this.countParas = 0;
+  },
 };
 
 let value = "";
@@ -184,6 +48,9 @@ function addElementToExpression() {
   expression.insert(value, type);
   value = "";
   type = "";
+}
+function displayExpression() {
+  display.textContent = expression.printExpression();
 }
 
 function enterNumber(char) {
@@ -199,7 +66,7 @@ function enterNumber(char) {
   }
 }
 
-function enterFloat(char) {
+function enterFloat() {
   //only if current type is number and if is negative must be numbers after -
   if (type === "number" && value !== "-") {
     if (value.includes(".")) return; //Gourd clause if number is already float
@@ -261,9 +128,34 @@ function enterClosePara(char) {
   }
 }
 
-function calcMathExpression() {}
-function deleteLastChar() {}
-function resetMathExpression() {}
+function calcMathExpression() {
+  //Special case if last input is number
+  if (type === "number") addElementToExpression();
+  //Calculate expression
+  //Display result
+  //Prepare state for new input
+}
+
+function deleteLastChar() {
+  if (type === "number" && value !== "") {
+    value = value.slice(0, -1);
+    return;
+  }
+  if (type === "" && expression.lastType() === "number") {
+    ({ value, type } = expression.removeLast());
+    alert(`${value} ${type}`);
+  }
+  //Current number
+  //delete current number until value === ""
+  //then if elements not empty take element from top and delete it
+  //if element taken from top is number delete step 1.
+}
+
+function resetCalculator() {
+  value = "";
+  type = "";
+  expression.resetExpression();
+}
 
 for (let operator of operators) {
   operator.addEventListener("click", (e) =>
@@ -278,3 +170,6 @@ openPara.addEventListener("click", (e) => enterOpenPara(e.target.textContent));
 closePara.addEventListener("click", (e) =>
   enterClosePara(e.target.textContent)
 );
+equal.addEventListener("click", calcMathExpression);
+deleteChar.addEventListener("click", deleteLastChar);
+reset.addEventListener("click", resetCalculator);

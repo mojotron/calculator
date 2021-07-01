5d4d57679ad52c4293a3826f9645b73ab0a2d9f8
mojotron <igormalj@gmail.com>
2021-07-01 22:12:15 +0200

    Add evaluatePostfix function - change stack object to class - change queue object to class - add doMath function

--- js/calculator.js
+++ js/calculator.js
@@ -1,37 +1,42 @@
 "use strict";
-const queueDS = {
-  queue: [],
-  isEmpty: function () {
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  isEmpty() {
     return this.queue.length === 0;
-  },
-  enqueue: function (value) {
+  }
+  enqueue(value) {
     this.queue.push(value);
     return this.queue.length;
-  },
-  dequeue: function () {
+  }
+  dequeue() {
     return this.queue.shift();
-  },
-};
+  }
+}
 
-const stackDS = {
-  stack: [],
-  isEmpty: function () {
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+  isEmpty() {
     return this.stack.length === 0;
-  },
-  push: function (value) {
+  }
+  push(value) {
     this.stack.push(value);
     return this.stack.length;
-  },
-  pop: function () {
+  }
+  pop() {
     return this.stack.pop();
-  },
-  top: function () {
+  }
+  top() {
     return this.stack[this.stack.length - 1];
-  },
-};
+  }
+}
 //10 + 3 * 5 / (16 - 4)
 //-(3+5)-(1+2)
 //-(-(3+5)-(1+2))
+//-(-(3+5)/2-(4-(9/6+3)))
 let temp = [
   { value: "-", type: "unaryOpe" },
   { value: "(", type: "openPara" },
@@ -41,21 +46,26 @@ let temp = [
   { value: "+", type: "operator" },
   { value: "5", type: "number" },
   { value: ")", type: "closePara" },
+  { value: "/", type: "operator" },
+  { value: "2", type: "number" },
   { value: "-", type: "operator" },
   { value: "(", type: "openPara" },
-  { value: "1", type: "number" },
+  { value: "4", type: "number" },
+  { value: "-", type: "operator" },
+  { value: "(", type: "openPara" },
+  { value: "9", type: "number" },
+  { value: "/", type: "operator" },
+  { value: "6", type: "number" },
   { value: "+", type: "operator" },
-  { value: "2", type: "number" },
+  { value: "3", type: "number" },
   { value: ")", type: "closePara" },
   { value: ")", type: "closePara" },
-  // { value: "/", type: "operator" },
-  // { value: "5", type: "number" },
+  { value: ")", type: "closePara" },
 ];
 
 function operatorPrecedence(operator) {
-  if (operator.value === "+" || operator.value === "-") return 1;
   if (operator.value === "x" || operator.value === "/") return 2;
-  if (operator.type === "unaryOpe") return 3;
+  if (operator.value === "+" || operator.value === "-") return 1;
 }
 
 function comparePrecedence(a, b) {
@@ -63,6 +73,8 @@ function comparePrecedence(a, b) {
 }
 // Shunting-yard algorithm
 function infixToPostfix(expression) {
+  const queueDS = new Queue();
+  const stackDS = new Stack();
   expression.forEach(function (element) {
     //Number and open para goes directly to queue
     if (element.type === "number") {
@@ -72,9 +84,7 @@ function infixToPostfix(expression) {
       stackDS.push(element);
       //Close para - enqueue all from stack until you reach open para
     } else if (element.type === "closePara") {
-      while (stackDS.top().type !== "openPara") {
-        queueDS.enqueue(stackDS.pop());
-      }
+      while (stackDS.top().type !== "openPara") queueDS.enqueue(stackDS.pop());
       stackDS.pop(); //Remove open para from stack
       //Unary - has higher precedence then other operators, directly psh to stack
     } else if (element.type === "unaryOpe") {
@@ -101,6 +111,32 @@ function infixToPostfix(expression) {
   });
   //Enqueue leftover operators from stack
   while (!stackDS.isEmpty()) queueDS.enqueue(stackDS.pop());
+  return queueDS;
 }
-infixToPostfix(temp);
-//unary opr if in evaulation 1 operand and 1 operator make calculation
+const test = infixToPostfix(temp);
+console.log(test.queue);
+function doMath(a, b, operator) {
+  if (operator === "+") return a + b;
+  if (operator === "-") return a - b;
+  if (operator === "x") return a * b;
+  if (operator === "/") return a / b;
+}
+
+function evaluatePostfix(expression) {
+  const numbers = new Stack();
+  expression.forEach(function (element) {
+    //If element is number push value converted from string to number
+    if (element.type === "number") numbers.push(+element.value);
+    //If element is operator pop last 2 numbers do operation with
+    //them end add result to stack
+    if (element.type === "operator") {
+      const num2 = numbers.pop();
+      const num1 = numbers.pop();
+      numbers.push(doMath(num1, num2, element.value));
+    }
+    //if element is unary operator multiply top with -1
+    if (element.type === "unaryOpe") numbers.push(numbers.pop() * -1);
+  });
+  return numbers;
+}
+console.log(evaluatePostfix(test.queue));
